44d0d7880e4859c19c6c1758c02615594b1dcfd1
junseongday <junseongday@gmail.com>
2020-02-11 23:24:58 +0900

    UIViewController subclassing ver

---
+++ Chapter03-CSStepper/CSStepper.swift
@@ -0,0 +1,124 @@
+//
+//  CSStepper.swift
+//  Chapter03-CSStepper
+//
+//  Created by JSMAC on 2020/02/02.
+//  Copyright © 2020 JSPRO. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+public class CSStepper: UIView {
+    
+    public var leftBtn = UIButton(type: .system) // 좌측 버튼
+    public var rightBtn = UIButton(type: .system) // 우측 버튼
+    public var centerLabel = UILabel() // 중앙 레이블
+    // 스테퍼의 현재값을 저장할 변수
+    @IBInspectable
+    public var value: Int = 0 {
+        didSet {
+            self.centerLabel.text = String(value)
+        }
+    }
+    
+    @IBInspectable
+    public var leftTitle: String = "↓" {
+        didSet {
+            self.leftBtn.setTitle(leftTitle, for: .normal)
+        }
+    }
+    
+    @IBInspectable
+    public var rightTitle: String = "↑" {
+        didSet {
+            self.rightBtn.setTitle(rightTitle, for: .normal)
+        }
+    }
+    
+    @IBInspectable
+    public var bgColor: UIColor = UIColor.cyan {
+        didSet {
+            self.centerLabel.backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable
+    public var stepValue: Int = 1
+    
+    @IBInspectable public var maximumValue: Int =  100
+    
+    @IBInspectable public var minimumValue: Int = -100
+    
+    // storyboard
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setup()
+    }
+    
+    // programing
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    private func setup() {
+        
+        let borderWidth: CGFloat = 0.5
+        let borderColor = UIColor.blue.cgColor
+        
+        self.leftBtn.tag = -1
+//        self.leftBtn.setTitle("↓", for: .normal)
+        self.leftBtn.setTitle(self.leftTitle, for: .normal)
+        self.leftBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        self.leftBtn.layer.borderWidth = borderWidth
+        self.leftBtn.layer.borderColor = borderColor
+        
+        self.rightBtn.tag = 1
+//        self.rightBtn.setTitle("↑", for: .normal)
+        self.rightBtn.setTitle(self.rightTitle, for: .normal)
+        self.rightBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        self.rightBtn.layer.borderWidth = borderWidth
+        self.rightBtn.layer.borderColor = borderColor
+        
+        self.centerLabel.text = String(self.value)
+        self.centerLabel.font = UIFont.systemFont(ofSize: 16)
+        self.centerLabel.textAlignment = .center
+//        self.centerLabel.backgroundColor = UIColor.cyan
+        self.centerLabel.backgroundColor = self.bgColor
+        self.centerLabel.layer.borderWidth = borderWidth
+        self.centerLabel.layer.borderColor = borderColor
+        
+        self.addSubview(self.leftBtn)
+        self.addSubview(self.rightBtn)
+        self.addSubview(self.centerLabel)
+        
+        self.leftBtn.addTarget(self, action: #selector(valueChange(_:)), for: .touchUpInside)
+        self.rightBtn.addTarget(self, action: #selector(valueChange(_:)), for: .touchUpInside)
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let btnWidth = self.frame.height
+        let lblWidth = self.frame.width - (btnWidth * 2)
+        
+        self.leftBtn.frame = CGRect(x: 0, y: 0, width: btnWidth, height: btnWidth)
+        self.centerLabel.frame = CGRect(x: btnWidth, y: 0, width: lblWidth, height: btnWidth)
+        self.rightBtn.frame = CGRect(x: btnWidth + lblWidth, y: 0, width: btnWidth, height: btnWidth)
+        
+    }
+    
+    
+    @objc func valueChange(_ sender: UIButton) {
+        let sum = self.value + sender.tag * self.stepValue
+        if sum > self.maximumValue {
+            return
+        }
+        if sum < self.minimumValue {
+            return
+        }
+        self.value += sender.tag * self.stepValue
+    }
+    
+
+}
